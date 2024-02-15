50299ef46f3ce051155f4dfb2dfbaecf86c6bc51
youtlane <youtlanehanane@gmail.com>
2024-02-15 12:10:42 +0100

    fix: add no rectte msg and dropdowns issues

--- script/pages/displayRecipes.js
+++ script/pages/displayRecipes.js
@@ -2,21 +2,22 @@ export function displayRecipes(recipesData) {
     const recipesContainer = document.querySelector('.main_content');
     recipesContainer.innerHTML = '';
 
-    recipesData.forEach(recipe => {
-        let sectionIngredient = '<div class="ingredient_content">';
+    if (recipesData.length > 0) {
+        recipesData.forEach(recipe => {
+            let sectionIngredient = '<div class="ingredient_content">';
 
-        recipe.ingredients.forEach(ingredient => {
-            sectionIngredient += `<div class="ingredient">
+            recipe.ingredients.forEach(ingredient => {
+                sectionIngredient += `<div class="ingredient">
                 <h2>${ingredient.ingredient}</h2>
                 <h3>${ingredient.quantity ? ingredient.quantity : ''} ${ingredient.unit ? ingredient.unit : ''}</h3>
             </div>`;
-        });
+            });
 
-        sectionIngredient += '</div>'
+            sectionIngredient += '</div>'
 
-        const listItem = `<div class="card">
+            const listItem = `<div class="card">
                 <p class='recipe-time'>${recipe.time} min</p>
-                <img src="assets/photos/${recipe.image}" alt="" class="card__pic">
+                <img src="assets/photos/${recipe.image}" alt="recipe image" class="card__pic">
                 <div class="card__content">
                     <h2 class="recipe-title">${recipe.name}</h2>
                     <div class="recipe-text">
@@ -27,6 +28,10 @@ export function displayRecipes(recipesData) {
                     </div>
                 </div>
             </div>`;
-        recipesContainer.insertAdjacentHTML('afterbegin', listItem);
-    });
+            recipesContainer.insertAdjacentHTML('afterbegin', listItem);
+        });
+    } else {
+        recipesContainer.insertAdjacentHTML('afterbegin',
+            "<div class='empty-recipes'>Aucune recette avec ces paramètres n'a été trouvée.</div>");
+    }
 }
